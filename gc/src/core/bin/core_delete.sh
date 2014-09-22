8d18f0a87a30032f9996087a492fa000a5bc1a14
guanzx <guanzx@funshion.com>
2014-09-22 15:15:01 +0800

    修改删除逻辑

--- gc/src/core/bin/core_delete.sh
+++ gc/src/core/bin/core_delete.sh
@@ -81,26 +81,100 @@ deleteHdfsModule() {
 	fi	
 }
 
-
 # 删除ETL过期数据
 deleteEtlDir() {
 	stat_date=$2
-	ymd=${stat_date:0:8}
+	yyyy=${stat_date:0:4}
+	mm=${stat_date:4:2}
+	dd=${stat_date:6:2}
 	hh=${stat_date:8:2}
-	format_stat_date=$(date -d "$ymd $hh" +%Y/%m/%d/%H)
+	format_stat_date=$(date -d "$yyyy$mm$dd $hh" +%Y/%m/%d/%H)
 	if [[ $? -ne 0 ]]
 	then
 		echo "ETL DELETE Error: $2 format error..."  >>$elog
 	else
-		rm_etl_dir=$1/${format_stat_date}
-		deleteHdfsModule $rm_etl_dir
+		monthList="$($HADOOP_INSTALL/bin/hadoop fs -ls $1/$yyyy 2>/dev/null | awk 'NR != 1{print $8}' | awk -F"/" '{print $7}')"
+		dayList="$($HADOOP_INSTALL/bin/hadoop fs -ls $1/$yyyy/$mm 2>/dev/null | awk 'NR != 1{print $8}' | awk -F"/" '{print $8}')"
+		hourList="$($HADOOP_INSTALL/bin/hadoop fs -ls $1/$yyyy/$mm/$dd 2>/dev/null | awk 'NR != 1{print $8}' | awk -F"/" '{print $9}')"
+		
+		if [[ $mm != 01 ]]
+		then
+			for m in $(seq -f %02g 01 $(( $((10#$mm)) - 1 )))
+			do
+				if echo $monthList | grep -q $m
+				then
+					deleteHdfsModule $1/$yyyy/$m
+				fi	
+			done
+		fi
+		
+		if [[ $dd != 01 ]]
+		then
+			for d in $(seq -f %02g 01 $(( $((10#$dd)) - 1 )) )
+			do
+				if echo $dayList | grep -q $d
+				then
+					deleteHdfsModule $1/$yyyy/$mm/$d
+				fi
+			done
+		fi
+		
+		for h in $(seq -f %02g 00 $hh)
+		do
+			if echo $hourList | grep -q $h
+			then
+				deleteHdfsModule $1/$yyyy/$mm/$dd/$h
+			fi
+		done
 	fi
 }
 
 # 删除除ETL之外的其他hdfs目录
-deleteHdfsDir() {
-	rm_hdfs_dir=$1/$3/$2
-	deleteHdfsModule $rm_hdfs_dir
+deleteHdfsDir() {	
+	
+	if [[ $3 == "day" ]]
+	then
+		stat_date=$2
+		yyyy=${stat_date:0:4}
+		mm=${stat_date:4:2}
+		dd=${stat_date:6:2}
+		dateList="$($HADOOP_INSTALL/bin/hadoop fs -ls $1/$3 2>/dev/null | awk 'NR != 1{print $8}' | awk -F"/" '{print $7}')"
+		
+		for m in $(seq -f %02g 01 $mm)
+		do
+			for d in $(seq -f %02g 01 $dd)
+			do
+				if echo $dateList | grep -q $yyyy$m$d
+				then
+					deleteHdfsModule $1/$3/$yyyy$m$d
+				fi
+			done
+		done
+	fi
+	
+	if [[ $3 == "hour" ]]
+	then
+		stat_date=$2
+		yyyy=${stat_date:0:4}
+		mm=${stat_date:4:2}
+		dd=${stat_date:6:2}
+		hh=${stat_date:8:2}
+		dateList="$($HADOOP_INSTALL/bin/hadoop fs -ls $1/$3 2>/dev/null | awk 'NR != 1{print $8}' | awk -F"/" '{print $7}')"
+		
+		for m in $(seq -f %02g 01 $mm)
+		do
+			for d in $(seq -f %02g 01 $dd)
+			do
+				for h in $(seq -f %02g 00 $hh)
+				do
+					if echo $dateList | grep -q $yyyy$m$d$h
+					then
+						deleteHdfsModule $1/$3/$yyyy$m$d$h
+					fi		
+				done		
+			done
+		done
+	fi
 }
 #-------------------- END ----------------------------------
 
@@ -123,8 +197,26 @@ deleteLocalModule() {
 
 # 删除本地过期数据
 deleteLocalDir() {
-	rm_local_dir=$1/$3/$2
-	deleteLocalModule $rm_local_dir
+	if [[ $3 == "day" ]]
+	then
+		for dayDate in `ls -A $1/$3`
+		do
+			if (( $dayDate <= $2 ))
+			then
+				deleteLocalModule $1/$3/$dayDate
+			fi
+		done
+	fi
+	if [[ $3 == "hour" ]]
+	then
+		for hourDate in `ls -A $1/$3`
+		do 
+			if (( $hourDate <= $2 ))
+			then
+				deleteLocalModule $1/$3/$hourDate
+			fi
+		done
+	fi
 }
 #------------- END -------------------------------------------
 if [[ $dir_env == "etl" ]]
